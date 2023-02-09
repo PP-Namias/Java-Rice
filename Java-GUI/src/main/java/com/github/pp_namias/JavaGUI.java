c0bee0a1eeb40095d8793c50c9d728e5fd23f308
Jhon Keneth Namias <75213409+PP-Namias@users.noreply.github.com>
2023-02-09 15:05:01 +0800

    :sparkles: Added First form :sparkles:

---
+++ Java-GUI/src/main/java/com/github/pp_namias/JavaGUI.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.github.pp_namias;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author jkrbn
+ */
+public class JavaGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
