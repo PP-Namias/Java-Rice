02d40bdfb82337d164ef9d9ebcc992e7a0103900
Jhon Keneth Namias <75213409+PP-Namias@users.noreply.github.com>
2023-02-13 22:28:06 +0800

    Alpha build design

---
+++ Java-Rice/src/main/java/com/github/pp_namias/java_rice/JavaRice.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.github.pp_namias.java_rice;
+
+/**
+ *
+ * @author jkrbn
+ */
+public class JavaRice {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
